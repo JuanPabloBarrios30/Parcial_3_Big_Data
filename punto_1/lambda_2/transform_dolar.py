aee862d490ac611252aace7c3b22ce9a31c8e175
Ubuntu <ubuntu@ip-172-31-51-235.ec2.internal>
2023-05-29 18:13:20 +0000

    Flake8 punto 1

--- punto_1/lambda_2/transform_dolar.py
+++ punto_1/lambda_2/transform_dolar.py
@@ -1,7 +1,7 @@
 import boto3
 import json
-import csv
 from datetime import datetime
+
 
 def lambda_handler(event, context):
     s3 = boto3.resource('s3')
@@ -9,29 +9,25 @@ def lambda_handler(event, context):
     obj = bucket.Object('dolar_timestamp.txt')
     body = obj.get()['Body'].read()
     print(json.loads(body))
-    archivo=json.loads(body)
-    
-    csv='Fecha,Dolar\n'
-    
-    for i in range(0,len(archivo)):
-        
-        timestap=int(archivo[i][0])/1000
+    archivo = json.loads(body)
+
+    csv = 'Fecha,Dolar\n'
+
+    for i in range(0, len(archivo)):
+
+        timestap = int(archivo[i][0])/1000
         dt_object = datetime.fromtimestamp(timestap)
-        csv+=str(dt_object)
-        csv+=','
-        csv+=str(archivo[i][1])
-        csv+='\n'
+        csv += str(dt_object)
+        csv += ','
+        csv += str(archivo[i][1])
+        csv += '\n'
 
     print(csv)
-    
-    
+
     s3 = boto3.resource('s3')
     object = s3.Object('transform-dolar-jp', 'dolar_timestamp.csv')
     object.put(Body=csv)
 
-
-    
-    
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
